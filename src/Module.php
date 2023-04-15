ea4fb52736a4f90afb9062f316ed7926a3ed1120
demirmehmet0 <demirmehmet0@yahoo.com>
2023-04-15 16:54:44 +0300

    refactor: change flash

--- src/Module.php
+++ src/Module.php
@@ -13,6 +13,8 @@ class Module extends \portalium\base\Module
         ],
     ];
     public static $tablePrefix = 'storage_';
+    public static $name = 'Storage';
+    
     public static function moduleInit()
     {
         self::registerTranslation('storage','@portalium/storage/messages',[
