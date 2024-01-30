dc53b1b0f653f07e2e6ce14b8350d944c59fce6c
demirmehmet0 <demirmehmet0@yahoo.com>
2024-01-30 09:45:47 +0300

    fix: rbac bug fixed

--- src/migrations/m220228_125709_storage_rule_rbac.php
+++ src/migrations/m220228_125709_storage_rule_rbac.php
@@ -18,8 +18,6 @@ class m220228_125709_storage_rule_rbac extends Migration
             'storageApiDefaultCreate' => 'Storage Api Default Create',
             'storageApiDefaultUpdate' => 'Storage Api Default Update',
             'storageApiDefaultDelete' => 'Storage Api Default Delete',
-            'storageApiDefaultIndex' => 'Storage Api Default Index',
-            'storageWebDefaultIndex' => 'Storage Web Default Index',
             'storageWebDefaultView' => 'Storage Web Default View',
             'storageWebDefaultCreate' => 'Storage Web Default Create',
             'storageWebDefaultUpdate' => 'Storage Web Default Update',
@@ -36,7 +34,18 @@ class m220228_125709_storage_rule_rbac extends Migration
             $auth->addChild($admin, $permissionForWorkspace);
             $permission = $auth->getPermission($permissionKey);
             $auth->addChild($permissionForWorkspace, $permission);
+        }
 
+        $permissions = [
+            'storageApiDefaultIndex' => 'Storage Api Default Index',
+            'storageWebDefaultIndex' => 'Storage Web Default Index',
+        ];
+
+        foreach ($permissions as $permissionKey => $permissionDescription) {
+            $permissionForWorkspace = $auth->createPermission($permissionKey . $endPrefix);
+            $permissionForWorkspace->description = ' (' . $endPrefix . ')' . $permissionDescription;
+            $auth->add($permissionForWorkspace);
+            $auth->addChild($admin, $permissionForWorkspace);
         }
     }
 
