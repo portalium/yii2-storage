feb415ee2b28045c0898783fc334d8f97819d393
Yakup Can GÖKDEMİR <Yakup.Can_Gokdemir@hotmail.com>
2026-03-12 09:16:35 +0300

    refactor: removed duplicate permissions, added only missing permission
    Checked duplicate permission errors during RBAC migration.
    Existing permissions in auth_item were not re-added, only the missing permission was created.

--- src/migrations/m250121_120002_storage_rule_rbac.php
+++ src/migrations/m250121_120002_storage_rule_rbac.php
@@ -11,38 +11,40 @@ class m250121_120002_storage_rule_rbac extends Migration
         $rule = new OwnRule();
         if (!$auth->getRule($rule->name)) {
             $auth->add($rule);
-        }
-
+        } 
         $role = Yii::$app->setting->getValue('site::admin_role');
         $admin = (isset($role) && $role != '') ? $auth->getRole($role) : $auth->getRole('admin');
         $user = $auth->getRole('user');
 
         $permissions = [
-            'storageWebDefaultUploadFile',
-            'storageWebDefaultDownloadFile',
-            'storageWebDefaultRenameFile',
-            'storageWebDefaultUpdateFile',
-            'storageWebDefaultShareFile',
-            'storageWebDefaultCopyFile',
-            'storageWebDefaultDeleteFile',
-            'storageWebDefaultPickerModal',
-            'storageWebDefaultFileList',
-            'storageWebDefaultSearch',
-            'storageWebDefaultNewFolder',
-            'storageWebDefaultRenameFolder',
-            'storageWebDefaultDeleteFolder',
             'storageWebDefaultdeleteFolderRecursive',
-            'storageWebDefaultManageDirectory',
-            'storageWebDefaultGetFile',
         ];
 
         foreach ($permissions as $permissionKey) {
+            $permissionOwn = $auth->createPermission($permissionKey . 'Own');
+            $description = preg_replace('/([a-z])([A-Z])/', '$1 $2', $permissionKey . 'Own');
+            $permissionOwn->description = ucfirst($description);
+            $permissionOwn->ruleName = $rule->name;
+            $auth->add($permissionOwn);
+            $auth->addChild($admin, $permissionOwn);
+            
+            if ($user) {
+                $auth->addChild($user, $permissionOwn);
+            }
+            
             $permission = $auth->getPermission($permissionKey);
             if (!$permission) {
                 $permission = $auth->createPermission($permissionKey);
                 $permission->description = preg_replace('/([a-z])([A-Z])/', '$1 $2', $permissionKey);
                 $permission->description = ucfirst($permission->description);
                 $auth->add($permission);
+            }
+            try {            $auth->addChild($permissionOwn, $permission);
+
+                //code...
+            } catch (\Throwable $th) {
+                
+                //throw $th;
             }
         }
     }
@@ -52,28 +54,13 @@ class m250121_120002_storage_rule_rbac extends Migration
         $auth = Yii::$app->authManager;
 
         $permissions = [
-            'storageWebDefaultUploadFile',
-            'storageWebDefaultDownloadFile',
-            'storageWebDefaultRenameFile',
-            'storageWebDefaultUpdateFile',
-            'storageWebDefaultShareFile',
-            'storageWebDefaultCopyFile',
-            'storageWebDefaultDeleteFile',
-            'storageWebDefaultPickerModal',
-            'storageWebDefaultFileList',
-            'storageWebDefaultSearch',
-            'storageWebDefaultNewFolder',
-            'storageWebDefaultRenameFolder',
-            'storageWebDefaultDeleteFolder',
             'storageWebDefaultdeleteFolderRecursive',
-            'storageWebDefaultManageDirectory',
-            'storageWebDefaultGetFile',
         ];
 
         foreach ($permissions as $permissionKey) {
-            $permission = $auth->getPermission($permissionKey);
-            if ($permission) {
-                $auth->remove($permission);
+            $permissionOwn = $auth->getPermission($permissionKey . 'Own');
+            if ($permissionOwn) {
+                $auth->remove($permissionOwn);
             }
         }
     }
